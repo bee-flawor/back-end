72d55f003eeda9050b1cdbfb3e96673cf37de79e
bee-flawor <141031374+bee-flawor@users.noreply.github.com>
2024-02-28 09:48:17 -0300

    Add files via upload
    aula dia 28-02

---
+++ aula 28-02/index.js
@@ -0,0 +1,40 @@
+function somar(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const total = num1 + num2;
+
+    console.log(`A soma ${num1} + ${num2} = ${total}`);   
+}
+
+function subitrair(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const total = num1 - num2;
+
+    console.log(`A soma ${num1} - ${num2} = ${total}`);   
+}
+
+function multiplicar(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const total = num1 * num2;
+
+    console.log(`A soma ${num1} * ${num2} = ${total}`);   
+}
+
+function dividir(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const total = num1 / num2;
+
+    console.log(`A soma ${num1} / ${num2} = ${total}`);   
+}
+
+somar();
+subitrair();
+multiplicar();
+dividir();
